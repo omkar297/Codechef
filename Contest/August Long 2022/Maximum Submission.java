8845874af8570c2c31b7d7f2e64c08c0f0f9210b
Omkar Rajesh Rathod <omkarrathod.297@gmail.com>
2022-08-14 18:27:25 +0530

    Solution for Maximum Submission in Java

---
+++ Contest/August Long 2022/Maximum Submission.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class MaximumSub{
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int testcase = input.nextInt();
+        while(testcase-- > 0){
+            int min = input.nextInt();
+            if(min >= 29){
+                System.out.println("0");
+            }else{
+                System.out.println(min * 2);
+            }
+        }
+    }
+}
